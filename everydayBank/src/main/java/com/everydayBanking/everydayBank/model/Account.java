34fb23d4811e5ffc925bdd79bafab2a9517da274
Jibril Richter <jibrilrichter@gmail.com>
2024-09-10 23:05:55 -0500

    edit Account class

--- everydayBank/src/main/java/com/everydayBanking/everydayBank/model/Account.java
+++ everydayBank/src/main/java/com/everydayBanking/everydayBank/model/Account.java
@@ -4,6 +4,7 @@ package com.everydayBanking.everydayBank.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "customer_account")
@@ -38,4 +39,76 @@ public class Account {
     }
 
 
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(Double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public LocalDateTime getCreation() {
+        return creation;
+    }
+
+    public void setCreation(LocalDateTime creation) {
+        this.creation = creation;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return accountId == account.accountId && customerId == account.customerId && Objects.equals(accountNumber, account.accountNumber) && Objects.equals(accountBalance, account.accountBalance) && Objects.equals(accountType, account.accountType) && Objects.equals(creation, account.creation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountId, accountNumber, accountBalance, accountType, creation, customerId);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId=" + accountId +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", accountBalance=" + accountBalance +
+                ", accountType='" + accountType + '\'' +
+                ", creation=" + creation +
+                ", customerId=" + customerId +
+                '}';
+    }
 }
--- everydayBank/src/main/java/com/everydayBanking/everydayBank/model/DashboardObject.java
+++ everydayBank/src/main/java/com/everydayBanking/everydayBank/model/DashboardObject.java
@@ -5,15 +5,18 @@ import java.util.List;
 public class DashboardObject {
     private String firstName;
     private String lastName;
+    private String jwtToken;
     private List<Account> customerAccounts;
 
     public DashboardObject() {
     }
 
-    public DashboardObject(String firstName, String lastName, List<Account> customerAccounts) {
+    public DashboardObject(String firstName, String lastName, String jwtToken, List<Account> customerAccounts) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.jwtToken = jwtToken;
         this.customerAccounts = customerAccounts;
+
     }
 
 }
